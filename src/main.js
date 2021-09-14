18ae4ff0f08f125412d5469681ac59a948a056da
Greg-One <sgmdoc@yandex.ru>
2021-09-14 21:41:26 +0500

    add custom vee-validation plugin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+
 import router from './router';
 import store from './store';
+import VeeValidatePlugin from './includes/validation';
 
 import './assets/tailwind.css';
 import './assets/main.css';
@@ -10,5 +12,6 @@ const app = createApp(App);
 
 app.use(store);
 app.use(router);
+app.use(VeeValidatePlugin);
 
 app.mount('#app');
