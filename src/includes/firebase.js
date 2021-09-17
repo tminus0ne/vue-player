e0e8d1cf656d84a922608acea671d3cdd6704b0c
Greg-One <sgmdoc@yandex.ru>
2021-09-17 13:41:45 +0500

    add Upload component & file uploading on drop

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBIk0-nUgg6_rxYXbwr4NIb-sVo1IvOcIE',
@@ -15,6 +16,7 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection('users');
 
@@ -22,4 +24,5 @@ export {
   auth,
   db,
   usersCollection,
+  storage,
 };
