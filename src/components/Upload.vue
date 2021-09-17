9f5fbb4080980ee917e2f54d38c7a245ee671495
Greg-One <sgmdoc@yandex.ru>
2021-09-17 16:12:05 +0500

    add song upload via drag&drop

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -25,26 +25,27 @@
     <hr class="my-6" />
 
     <!-- Progess Bars -->
-    <div class="mb-4">
+    <div
+      class="mb-4"
+      v-for="upload in uploads"
+      :key="upload.name"
+    >
 
       <!-- File Name -->
-      <div class="font-bold text-sm">Just another song.mp3</div>
+      <div
+        class="font-bold text-sm"
+        :class="upload.textClass"
+      >
+        <i :class="upload.icon"></i> {{ upload.name }}
+      </div>
       <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
 
         <!-- Inner Progress Bar -->
-        <div class="transition-all progress-bar bg-blue-400" style="width: 75%"></div>
-      </div>
-    </div>
-    <div class="mb-4">
-      <div class="font-bold text-sm">Just another song.mp3</div>
-      <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-        <div class="transition-all progress-bar bg-blue-400" style="width: 35%"></div>
-      </div>
-    </div>
-    <div class="mb-4">
-      <div class="font-bold text-sm">Just another song.mp3</div>
-      <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-        <div class="transition-all progress-bar bg-blue-400" style="width: 55%"></div>
+        <div
+          class="transition-all progress-bar"
+          :class="upload.variant"
+          :style="{ width: upload.currentProgress + '%' }"
+        ></div>
       </div>
     </div>
   </div>
@@ -52,13 +53,14 @@
 </template>
 
 <script>
-import { storage } from '@/includes/firebase';
+import { storage, auth, songsCollection } from '@/includes/firebase';
 
 export default {
   name: 'Upload',
   data() {
     return {
       isDragover: false,
+      uploads: [],
     };
   },
   methods: {
@@ -74,7 +76,45 @@ export default {
 
         const storageRef = storage.ref(); // music-26cbb.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`);
-        songsRef.put(file);
+        const task = songsRef.put(file);
+
+        const uploadIndex = this.uploads.push({
+          task,
+          currentProgress: 0,
+          name: file.name,
+          variant: 'bg-blue-400',
+          icon: 'fas fa-spinner fa-spin',
+          textClass: '',
+        }) - 1;
+
+        task.on('stateChanged',
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            this.uploads[uploadIndex].currentProgress = progress;
+          },
+          (error) => {
+            this.uploads[uploadIndex].variant = 'bg-red-400';
+            this.uploads[uploadIndex].icon = 'fas fa-times';
+            this.uploads[uploadIndex].textClass = 'text-red-400';
+            console.log(error);
+          },
+          async () => {
+            const song = {
+              uid: auth.currentUser.uid,
+              displayName: auth.currentUser.displayName,
+              originalName: task.snapshot.ref.name,
+              modifiedName: task.snapshot.ref.name,
+              genre: '',
+              commentCount: 0,
+            };
+
+            song.url = await task.snapshot.ref.getDownloadURL();
+            await songsCollection.add(song);
+
+            this.uploads[uploadIndex].variant = 'bg-green-400';
+            this.uploads[uploadIndex].icon = 'fas fa-check';
+            this.uploads[uploadIndex].textClass = 'text-green-400';
+          });
       });
       console.log(files);
     },
