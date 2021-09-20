cc114d91977be940c5ab6c8c8c242c8f32f577f9
Greg-One <sgmdoc@yandex.ru>
2021-09-20 21:29:26 +0500

    add directives testing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,10 +24,13 @@
     <!-- Main Content -->
     <section class="container mx-auto">
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+      <div
+        class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
+        v-icon-secondary="{ icon: 'headphones-alt', right: true }"
+      >
+      <!-- v-icon.right.yellow="'headphones-alt'" -->
         <span class="card-title">Songs</span>
         <!-- Icon -->
-        <i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>
       </div>
 
       <!-- Playlist -->
@@ -47,11 +50,15 @@
 <script>
 import { songsCollection } from '@/includes/firebase';
 import AppSongItem from '@/components/SongItem.vue';
+import IconSecondary from '@/directives/icon-secondary';
 
 export default {
   name: 'Home',
   components: {
     AppSongItem,
+  },
+  directives: {
+    'icon-secondary': IconSecondary,
   },
   data() {
     return {
